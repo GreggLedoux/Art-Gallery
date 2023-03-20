82a835dfd27f33d97b829c5400874ea204ddbf8c
GreggLedoux <guitlouie@gmail.com>
2023-03-20 19:51:46 -0400

    Art Gallery mostly done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import Gallery from './components/Gallery'
+import ButtonBar from './components/ButtonBar'
 
 function App() {
+  let [artDetails, setArtDetails] = useState({})
+  let [objectId, setObjectId] = useState(12770)
+
+  useEffect(() => {
+    document.title="Welcome to Artworld";
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+    .then(response => response.json())
+    .then(resartDetails => setArtDetails(resartDetails))
+  }, [objectId])
+
+  const handleIterate = (e) => {
+    setObjectId(objectId + Number(e.target.value))
+  }
+
+  const displayImage = () => {
+    if(!artDetails.primaryImage) {
+      return (
+        <h2>No Image!</h2>
+      )
+    }
+    return (
+      <Gallery objectImg={artDetails.primaryImage} title={artDetails.title} />
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{artDetails.title}</h1>
+      <div style={{'width': '100%'}}>
+        {displayImage()}
+      </div>
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
 
 export default App;
+
+
+
+   
